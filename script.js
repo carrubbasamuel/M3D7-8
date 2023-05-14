1e6fc2161cae8d2ad2bba4a89b717903f46eda39
Samuel Carrubba <97916309+carrubbasamuel@users.noreply.github.com>
2023-05-14 03:27:27 +0200

    finish

---
+++ script.js
@@ -0,0 +1,45 @@
+let buttonPrevs = document.querySelectorAll('#carousel button[data-bs-slide="prev"]');
+let buttonNexts = document.querySelectorAll('#carousel button[data-bs-slide="next"]');
+
+
+function hiddenButton() {
+    for (const buttonNext of buttonNexts) {
+        buttonNext.style.visibility = 'hidden';
+    }
+    for (const buttonPrev of buttonPrevs) {
+        buttonPrev.style.visibility = 'hidden';
+    }
+}
+
+function showButton() {
+    for (const buttonNext of buttonNexts) {
+        buttonNext.style.visibility = 'visible';
+    }
+    for (const buttonPrev of buttonPrevs) {
+        buttonPrev.style.visibility = 'visible';
+    }
+}
+
+
+function main(){
+    let audio = document.getElementById("myAudio");
+    audio.currentTime = 0;
+    audio.play();
+    for (const buttonPrev of buttonPrevs) {
+        buttonPrev.addEventListener('click', () => {
+            hiddenButton();
+            setTimeout(showButton, 500);
+        });
+    }
+    for (const buttonNext of buttonNexts) {
+        buttonNext.addEventListener('click', () => {
+            hiddenButton();
+            setTimeout(showButton, 1000);
+        });
+    }
+}
+
+
+
+/* Funzione per il comportamento dei buttons del carousel */
+window.onload = main;
